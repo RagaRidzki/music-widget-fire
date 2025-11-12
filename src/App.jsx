a45243b59a76a5f35d432a6adb51095b1a70d233
RagaRidzki <ragaridzkyy@gmail.com>
2025-11-12 21:17:00 +0700

    delete: remove supabase on project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 // src/App.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Create from "./pages/Create";
 import Widget from "./pages/Widget";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/create" element={<Create />} />
         <Route path="/:slug" element={<Widget />} />
-        <Route path="*" element={<Create />} />
       </Routes>
     </BrowserRouter>
   );
